0f0afe4856658c04bece9016a659035440cfa705
Gobans <audghks4452@gmail.com>
2020-07-17 17:37:16 +0900

    first

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength :50
+    },
+    email:{
+        type : string,
+        trime : true,
+        unique: 1
+    },
+    lastame :{
+        type: String,
+        maxlength :30
+    },
+    role:{
+        type: Number,
+        default : 0
+    },
+    image: String,
+    token: {
+        type : String
+    },
+    tokneExp:{
+        type : Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+models.exports = {User}
